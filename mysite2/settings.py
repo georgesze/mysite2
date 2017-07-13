58a1ae29f15437fd003dae2033bee5bc3504506f
georgesze <40915864@qq.com>
2017-07-13 16:08:33 +0800

    11
    111

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!^*&7^!y&g880pig=fkupher&xfe6dm9uysm*9!6&6to6@b7@7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.pythonanywhere.com']
 
 
 # Application definition
