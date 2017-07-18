a71be1f666373e2b53107332ac22d556486d56cf
georgesze <40915864@qq.com>
2017-07-18 10:24:21 +0800

    111
    222

--- disk/upld.py
+++ disk/upld.py
@@ -32,7 +32,9 @@ def upld(request):
             #headImg = uf.cleaned_data['headImg']
 			# write to database
             time1 = time.time()
-            f = open(request.FILES['file'])
+			myfile = request.FILES['file']
+			# 打开文件
+            f = open(myfile)
             #print u"读取文件结束,开始导入!"
             time2 = time.time()
             WorkList = []
