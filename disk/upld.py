cb16ad93859261b46873778ccb5952e18f63b3f2
georgesze <40915864@qq.com>
2017-07-18 11:55:02 +0800

    123
    123

--- disk/upld.py
+++ disk/upld.py
@@ -68,7 +68,7 @@ def upld(request):
 
 def handle_uploaded_file(f):
     with open('some/file/name.txt', 'wb+') as destination:
-    for chunk in f.chunks():
-        destination.write(chunk)
+        for chunk in f.chunks():
+            destination.write(chunk)
 
 
