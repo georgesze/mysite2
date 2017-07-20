2bb29fc92870f5c587239f08b646478df13a16c5
georgesze <40915864@qq.com>
2017-07-20 17:59:46 +0800

    123
    123

--- disk/models.py
+++ disk/models.py
@@ -10,8 +10,13 @@ class User(models.Model):
 		
 		
 class Alimama(models.Model):
-    pid = models.CharField(max_length = 30)
-    commission = models.DecimalField(max_digits=5, decimal_places=2)
+    order = models.CharField(max_length = 20, primary_key=True, verbose_name='订单编号')
+    pid = models.CharField(max_length = 30, verbose_name='推广位')
+    commission = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='结算佣金')
 	
     def __str__(self):
         return self.pid
+    
+    class meta:
+        verbose_name = "推广订单"
+        verbose_name_plural = "推广订单"
