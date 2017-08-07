5dc8705f1222c7a32bad78d76ac7999ffb11fe20
georgesze <40915864@qq.com>
2017-08-07 15:33:19 +0800

    123
    1233

--- mysite2/search2.py
+++ mysite2/search2.py
@@ -26,18 +26,21 @@ def AgentList(request):
             end = form.cleaned_data.get('period_end')
             request.session['end'] = str(end)
             
-            #计算佣金
+            #html request 不支持python date格式，需要转换 ????
             start = request.session.get('start')
             end = request.session.get('end')
-        
             start = datetime.datetime.strptime(start, "%Y-%m-%d").date()
             end = datetime.datetime.strptime(end, "%Y-%m-%d").date()
-        
-            agent_orders = AliOrd.objects.filter(SettleDate__range=(start, end))            
-            
+
+            #取到所有代理列表            
             #agent_list = AliConfig.objects.filter()
             agent_list = AliConfig.objects.all()
-        
+            
+            for agent in agent_list:
+                # 计算所有订单佣金 volume 2000+
+                CalculateAgentOrder(agent,start,end)
+                
+                
     else:
         form = SearchForm()
      
@@ -88,3 +91,28 @@ def Agent(request, agent_name_slug):
     return render(request, 'order.html', context_dict)
 
 
+def CalculateAgentOrder(agent,start,end):
+    agent_pid = agent.AgentId.AgentId
+    orders = AliOrd.objects.filter(PosID=agent_pid,SettleDate__range=(start, end))
+               
+    for order_item in orders:  
+        #取得上线信息
+        order_tiem.UplineId = agent.AgentId.AgentUpId           #上线ID
+        order_tiem.UplineName = agent.AgentId.AgentName         #上线名称
+        #order_tiem.Up2lineId = agent.AgentId.                     #上上线ID
+        order_tiem.IncomePercSelf = agent.AgentPerc             #自获佣金比例
+        order_tiem.SharePercUp1 = agent.Agent2rdPerc            #贡献上级佣金比例
+        order_tiem.SharePercUp2 = agent.Agent3rdPerc    #贡献上上级佣金比例
+        # 计算佣金分成 ---- 计算使用SettleAmt结算金额 ----
+        order_tiem.IncomeSelf = order_item * agent.AgentPerc    #自获佣金
+        order_tiem.ShareUp1 = order_item * agent.Agent2rdPerc   #贡献上级佣金
+        order_tiem.ShareUp2 = order_item * agent.Agent3rdPerc   #贡献上上级佣金
+
+        #计算个人所得佣金
+
+        #计算上线分成佣金
+  
+        #计算上上线分成佣金
+        
+        #保存计算结果
+        order_item.save()
