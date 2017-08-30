6f16eeffaef7cd8b71819761ff0494cb56347f8d
georgesze <40915864@qq.com>
2017-08-31 00:25:21 +0800

    1
    1

--- static/js/test.js
+++ static/js/test.js
@@ -1,31 +1,32 @@
-$(function () {
+//$(function () {
+//
+//    //1.初始化Table
+//    var oTable = new TableInit();
+//    oTable.Init();	
+//	
+//    oTable
+//    
+//    
+//});
+//
+//var TableInit = function () {
+//    var oTableInit = new Object();
+//    //初始化Table
+//    oTableInit.Init = function () {
+//    	
+//
+//$('#agent_table').bootstrapTable({
+//    url: ActionUrl + 'GetMenuList',
+//    toolbar: '#toolbar',
+//    sidePagination: 'client',
+//    pagination: false,
+//    treeView: true,
+//    treeId: "Id",
+//    treeField: "Name",
+//    treeRootLevel: 1,
+//    clickToSelect: true,//collapseIcon: "glyphicon glyphicon-triangle-right",//折叠样式
+//    //expandIcon: "glyphicon glyphicon-triangle-bottom"//展开样式
+//});
+//}
 
-    //1.初始化Table
-    var oTable = new TableInit();
-    oTable.Init();	
-	
-    oTable
     
-    
-});
-
-var TableInit = function () {
-    var oTableInit = new Object();
-    //初始化Table
-    oTableInit.Init = function () {
-    	
-
-$('#tb_departments').bootstrapTable({
-    url: ActionUrl + 'GetMenuList',
-    toolbar: '#toolbar',
-    sidePagination: 'client',
-    pagination: false,
-    treeView: true,
-    treeId: "Id",
-    treeField: "Name",
-    treeRootLevel: 1,
-    clickToSelect: true,//collapseIcon: "glyphicon glyphicon-triangle-right",//折叠样式
-    //expandIcon: "glyphicon glyphicon-triangle-bottom"//展开样式
-});
-
-    }
