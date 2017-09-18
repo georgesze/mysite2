acfc168f9c93bbc0c134d108cbb19e4809dad2dc
georgesze <40915864@qq.com>
2017-09-18 22:32:10 +0800

    1
    1

--- mysite2/wsgi.py
+++ mysite2/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
+sys.path.append('/usr/lib/python3.5/site-packages')
 
 import os
 
@@ -15,4 +16,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite2.settings")
 
 application = get_wsgi_application()
 
-sys.path.append('/usr/lib/python3.5/site-packages')
