179a0b6f7c4f63e242fee1f95d88b60e6c5b7971
georgesze <40915864@qq.com>
2017-09-18 23:04:04 +0800

    1
    1

--- mysite2/wsgi.py
+++ mysite2/wsgi.py
@@ -6,10 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-import sys
-import os
-sys.path.append('/usr/lib/python3.5/site-packages')
 
+import os
 
 from django.core.wsgi import get_wsgi_application
 
